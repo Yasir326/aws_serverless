7c4c014a691fd569b7f24420861865a2e854a0d5
Yasir326 <yasirk@nandos.co.uk>
2020-11-15 15:15:41 +0000

    added get functionality

--- handlers/get_course_stats_handler.js
+++ handlers/get_course_stats_handler.js
@@ -1,23 +1,27 @@
 "use strict";
 const AWS = require("aws-sdk");
 const db = new AWS.DynamoDB.DocumentClient({ apiVersion: "2012-08-10" });
-const {response, sortByDate } = require("./helper_functions");
+const {response} = require("./helper_functions");
 
 const postsTable = process.env.POSTS_TABLE;
 
 module.exports.getCourseStats = (event, context, callback) => {
- const courseId = event.pathParameters.courseId;
- const params = {
-   TableName: postsTable,
-   Limit: courseId,
- };
+  const { courseId } = event.pathParameters;
+  const params = {
+    TableName: postsTable,
+    Key: {
+      id: courseId,
+    },
+  };
+
   return db
-    .scan({
-      params,
-    })
+    .get(params)
     .promise()
     .then((res) => {
-      callback(null, response(200, res.Items.sort(sortByDate)));
-    })
-    .catch((err) => callback(null, response(err.statusCode, err)));
+      if (res.item) {
+        callback(null, response(200, res.Item));
+      } else {
+        callback(null, response(404, { error: "CourseID not found" }));
+      }
+    });
 };
---
+++ handlers/get_session_id_stats_handler.js
@@ -0,0 +1,22 @@
+"use strict";
+const AWS = require("aws-sdk");
+const db = new AWS.DynamoDB.DocumentClient({ apiVersion: "2012-08-10" });
+const { response, sortByDate } = require("./helper_functions");
+
+const postsTable = process.env.POSTS_TABLE;
+
+module.exports.getCourseStats = (event, context, callback) => {
+  const { courseId, sessionId } = event.pathParameters;
+  const params = {
+    TableName: postsTable,
+    Key: {
+      id: courseId,
+    },
+  };
+
+  return db.get(params).promise().then(res => {
+      if(res.item) {callback(null, response(200, res.Item));} else {
+          callback(null, response(404, {error: "CourseID not found"}))
+      }
+  })
+};
